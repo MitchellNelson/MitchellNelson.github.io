f631f20fcc03bd8503d94d72e9d0ad82ff66a48b
Mitchell Nelson <nels4929@stthomas.edu>
2021-01-13 19:48:16 -0600

    Format Project images to same aspect ratio

--- src/Projects.js
+++ src/Projects.js
@@ -5,7 +5,7 @@ function Projects (props){
   var drumCV = {
     title: "drumCV",
     tech: "Python, Open CV",
-    img: "https://camo.githubusercontent.com/a2e13b8e2daa70a98e7f459fee866e7efc760cd011ddee6969aedeede42d9a0a/68747470733a2f2f696d672e796f75747562652e636f6d2f76692f4e7446334144765a4250732f302e6a7067",
+    img: "images/drumCV.png",
     gitHubLink: "https://github.com/MitchellNelson/drumCV",
     demoLink: "https://www.youtube.com/watch?v=NtF3ADvZBPs",
     description: "Computer vision drum simulator that produces drum sounds while a user is 'air drumming' with two colored drum sticks in front of a webcam."
@@ -13,18 +13,18 @@ function Projects (props){
   var beatCollab = {
     title: "beat-collab",
     tech: "Javascript, Vue.js, Node.js, Websockets",
-    img: "https://github.com/nels4929/beat-collab/raw/master/public/images/Screenshot.png",
+    img: "images/beatCollab.png",
     gitHubLink: "https://github.com/MitchellNelson/beat-collab",
     demoLink: "http://beatcollab.live",
     description: "Collaborative music creator that gives multiple users the power to create a drum beat in real time without being in the same room."
   };
   var reactDrumMachine = {
-    title: "React Drum Machine",
+    title: "Sequencer",
     tech: "React.js, HTML, CSS",
     img: "images/reactdrummachine.png",
     gitHubLink: "https://github.com/MitchellNelson/ReactDrumMachine",
     demoLink: "https://mitchellnelson.github.io/ReactDrumMachine/",
-    description: "Classic drum sequencer built with React.js as a way to learn about the framework"
+    description: "Classic drum sequencer built with React.js.                                                                                   "
   };
   var snapArt = {
     title: "snap-art",
@@ -32,7 +32,7 @@ function Projects (props){
     img: "images/gallary.jpg",
     gitHubLink: "https://github.com/RileyTschumper/MinneHack2020",
     demoLink: "https://www.youtube.com/watch?v=MC2t7IUA2lI&feature=emb_title",
-    description: "MinneHack 2020 3rd place winner. Users can snap a photo of a peice of art and the application will identify it"
+    description: "MinneHack 2020 3rd place winner. Provides artwork identification by using a scale-invariant feature transform (SIFT) algorithm."
   };
   return(
     <div class="projects center-column">
